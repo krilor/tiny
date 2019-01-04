c5537089983dde7bcdf428aa4c12352473926dfc
Kristoffer Lorentsen <krilor@gmail.com>
2019-01-04 17:08:49 +0100

    Add color and fontsize-support

--- template-blank.php
+++ template-blank.php
@@ -4,7 +4,7 @@ Template Name: Blank
 Template Post Type: post, page
 */
 
-// This file is supposed to be empty. It is just a way to give the user to choose a blank slate when creating files
+// This file is supposed to be empty. It is just a way to give the user to choose a blank slate when creating content.
 get_template_part('index');
 
 ?>
